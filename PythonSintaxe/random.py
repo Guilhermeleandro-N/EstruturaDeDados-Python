480afe038db8be0aad7dc8672e7c3b68188b86f4
Guilhermeleandro-N <135875294+Guilhermeleandro-N@users.noreply.github.com>
2024-04-02 13:32:53 -0300

    Create random.py

---
+++ PythonSintaxe/random.py
@@ -0,0 +1,22 @@
+import random 
+
+def main():
+    
+    smaller= int(input("Informe o menor valor: "))
+    larger = int(input("Informe o maior valor: "))
+    myNumber = random.randint(smaller, larger)
+    
+    count = 0
+    while True:
+        count += 1
+        userNumber = int(input("Qual seu chute? "))
+        if userNumber<myNumber:
+            print("Valor muito baixo!")
+        elif userNumber>myNumber:
+            print("Valor muito alto!")
+        else:
+            print(f"Você acertou em {count} tentativas!")
+          
+if __name__ == "__main__":
+    main()
+        
