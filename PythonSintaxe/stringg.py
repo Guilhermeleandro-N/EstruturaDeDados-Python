98ba4427acc18f57e116ed590b8d6e81938c5102
Guilhermeleandro-N <135875294+Guilhermeleandro-N@users.noreply.github.com>
2024-04-02 14:57:52 -0300

    Add files via upload

---
+++ PythonSintaxe/stringg.py
@@ -0,0 +1,14 @@
+"""a=input("Nome: ")
+b=input("Sobrenome: ")
+c=a+b
+print(c)
+d="Ana"
+print(d=="ana")
+print(d[2])
+"""
+"""print("greater"[:])
+print("greater"[2:])
+print("greater"[:2])
+print("greater"[2:5])"""
+a="peed"
+print(a*5)
